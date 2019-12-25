bd43b5274826bb440898679a6f3b9f5d50545eb1
Andrew Lingenfelter <luridsorcerer@gmail.com>
2019-12-24 23:34:54 -0500

    Fixed NMI routine, now able to draw sprites

--- src/test.asm
+++ src/test.asm
@@ -17,6 +17,7 @@
 	stx PPUCTRL
 	stx PPUMASK
 	stx APUSTATUS
+	stx $4010
 
 :	bit PPUSTATUS	; Wait for PPU to warm up
 	bpl :-
@@ -24,14 +25,17 @@
 	bpl :-
 
 	txa				; Loop through memory and zero it out
-:	sta $000, x
-	sta $100, x
-	sta $200, x
-	sta $300, x
-	sta $400, x
-	sta $500, x
-	sta $600, x
-	sta $700, x
+:	lda #$00
+	sta $0000, x
+	sta $0100, x
+	;sta $0200, x
+	sta $0300, x
+	sta $0400, x
+	sta $0500, x
+	sta $0600, x
+	sta $0700, x
+	lda #$fe		; Except sprite locations, move them offscreen
+	sta $0200, x
 	inx
 	bne :-
 
@@ -57,24 +61,23 @@
 	sta PPUADDR
 	
 	ldx #$00			; load palette
-PaletteLoop:
-	lda PaletteData, x
+:	lda PaletteData, x
 	sta PPUDATA
 	inx
 	cpx #$20
-	bne PaletteLoop
+	bne :-
 
-	lda #$80
+	lda #$80			; put a sprite on screen
 	sta $0200
 	sta $0203
 	lda #$00
 	sta $0201
 	sta $0202
 
-	lda #%10000000
+	lda #%10000000		; enable NMI
 	sta PPUCTRL
 
-	lda #%00010000
+	lda #%00010000		; enable sprites
 	sta PPUMASK
 
 forever:			; loop forever, nothing else to do
@@ -83,14 +86,14 @@ forever:			; loop forever, nothing else to do
 
 PaletteData:
 	.byte $1d,$20,$21,$22, $23,$24,$25,$26, $27,$28,$29,$2a, $2b,$2b,$2c,$2d
-	.byte $1d,$20,$21,$22, $23,$24,$25,$26, $27,$28,$29,$2a, $2b,$2b,$2c,$2d
+	.byte $1c,$20,$21,$22, $23,$24,$25,$26, $27,$28,$29,$2a, $2b,$2b,$2c,$2d
 
 .proc nmi			; NMI interrupt
 
 	lda #$00		; load sprites, low byte
 	sta OAMADDR
 	lda #$02		; high byte
-	sta OAMDATA
+	sta OAMDMA
 	rti
 
 .endproc
@@ -110,3 +113,4 @@ PaletteData:
 .incbin "test.chr"
 
 .segment "CHR0b"
+.incbin "test.chr"
