74c4200d75e39593e2af63ae511c506bca2eeaad
Andrew Lingenfelter <luridsorcerer@gmail.com>
2019-12-29 21:50:06 -0500

    added a message to the background, updated attribute table accordingly

--- src/attribtable.asm
+++ src/attribtable.asm
@@ -1,1 +1,5 @@
-	.byte %00000000, %00010000, %00100000, %00010000, %00000000, %00000000, %00110000, %00000000 
+; Each byte represents the palette attributes for a 4x4 set of tiles
+; They are subdivided into 2x2 tile squares, with two bits each.
+; BR -  BL - TR - TL
+
+	.byte %00000000, %00000000, %10000000, %10100000, %00100000, %01000000, %01010000, %00000000 
